68e523ecb0731c597f4120f5c0b1fe3fec9cb5e0
aki366 <96239914+aki366@users.noreply.github.com>
2023-12-30 03:30:56 +0900

    レコードに紐づくカテゴリ、言語、技術しか表示しないように設定

--- myapp/admin.py
+++ myapp/admin.py
@@ -6,6 +6,18 @@ class CareerlogAdmin(admin.ModelAdmin):
     list_filter = ['execution_date']                         # フィルター項目
     search_fields = ['title', 'description']                 # 検索可能なフィールド
 
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name in ['categories', 'languages', 'technologies']:
+            # 新しいCareerlogを追加する場合はNone
+            obj_id = request.resolver_match.kwargs.get('object_id')
+            if obj_id:
+                # 既存のCareerlogを編集する場合
+                kwargs["queryset"] = db_field.related_model.objects.filter(careerlog__id=obj_id)
+            else:
+                # 新しいCareerlogを追加する場合
+                kwargs["queryset"] = db_field.related_model.objects.none()
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
 admin.site.register(Careerlog, CareerlogAdmin)
 admin.site.register(Language)
 admin.site.register(Technology)
--- myapp/models.py
+++ myapp/models.py
@@ -23,9 +23,9 @@ class Careerlog(models.Model):
     description = models.TextField()
     execution_date = models.DateField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    categories = models.ManyToManyField(Category)
-    languages = models.ManyToManyField(Language)
-    technologies = models.ManyToManyField(Technology)
+    categories = models.ManyToManyField(Category, blank=True)
+    languages = models.ManyToManyField(Language, blank=True)
+    technologies = models.ManyToManyField(Technology, blank=True)
 
     def __str__(self):
         return self.title
