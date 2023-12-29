d407256effa713a258acea8435678af369ee22d6
aki366 <96239914+aki366@users.noreply.github.com>
2023-12-30 03:37:20 +0900

    カテゴリ、言語、技術にユニーク制約を追加

--- myapp/models.py
+++ myapp/models.py
@@ -1,19 +1,19 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
         return self.name
 
 class Language(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
         return self.name
 
 class Technology(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(unique=True, max_length=100)
 
     def __str__(self):
         return self.name
