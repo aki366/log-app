85e4632eb65f0cdbee9ba0c4864cda57176df1e6
aki366 <96239914+aki366@users.noreply.github.com>
2023-12-29 21:47:17 +0900

    リスト表示、フィルター、検索フィールド追加

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Careerlog
 
-admin.site.register(Careerlog)
+class CareerlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'execution_date', 'timestamp')  # リストで表示するフィールド
+    list_filter = ['execution_date']                         # フィルター項目
+    search_fields = ['title', 'description']                 # 検索可能なフィールド
+
+admin.site.register(Careerlog, CareerlogAdmin)
